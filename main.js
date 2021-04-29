1cd6af455419791460bc94e58f72aec867abe6fa
alexcoach <mvdrf030@gmail.com>
2021-04-29 17:38:56 +0200

    l5

--- main.js
+++ main.js
@@ -1,26 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const goods = [
-        { title: 'Shirt', img: 'http://via.placeholder.com/150/0000FF', price: 150 },
-        { title: 'Socks', img: 'http://via.placeholder.com/150/FF0000', price: 50 },
-        { title: 'Jacket', img: 'http://via.placeholder.com/150/FFFFFF', price: 350 },
-        { title: 'Shoes', img: 'http://via.placeholder.com/150/808080', price: 250 },
-    ];
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-    const renderGoodsItem = (item, button = '<button class="addCart">Добавить в корзину</button>') => {
-        return `'<div class="goods-item">
-                    <h3>${item.title}</h3>
-                    <img src="${item.img}">
-                    <p>Цена: ${item.price}</p>
-                    ${button}
-                </div>`
-
-    };
-
-    const renderGoodsList = list => {
-        document.querySelector('.goods-list').innerHTML = list.map(item => renderGoodsItem(item)).join('');
-    };
-
-    renderGoodsList(goods);
-})
-
-
+const app = new Vue({
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        baksetUrl: '/getBasket.json',
+        imgCatalog: 'https://placehold.it/200x150',
+        userSearch: '',
+        show: false,
+        filtered: [],
+        basket: []
+    },
+    methods: {
+        getJson(url){
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product){
+            let find = this.basket.find(el => el.id_product === product.id_product);
+            if(find){
+                find.quantity++;
+            } else {
+                const prod = Object.assign({quantity: 1}, product);//создание нового объекта на основе двух, указанных в параметрах
+                this.basket.push(prod)
+            }
+        },
+        removeProduct(product){
+            let item = this.basket.indexOf(product);
+            if(this.basket[item].quantity > 1)
+                this.basket[item].quantity--;
+            else {
+                this.basket.splice(item, 1);
+            }
+        },
+        filter(value){
+            const regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(product => regexp.test(product.product_name));
+        }
+    },
+    mounted(){
+        this.getJson(`${API + this.baksetUrl}`)
+            .then(data => {
+                for (let item of data.contents){
+                    this.basket.push(item);
+                }
+            });
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let item of data){
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+        this.getJson(`getProducts.json`)
+            .then(data => {
+                for(let item of data){
+                    this.products.push(item);
+                    this.filtered.push(item);
+                }
+            })
+    }
+});
